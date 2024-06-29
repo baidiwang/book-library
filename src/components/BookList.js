cd48672e1678806c544a0e9cf6efe6948f8bf5b5
LunaY0229 <55680212+baidiwang@users.noreply.github.com>
2024-06-29 10:43:07 +0800

    update book list style

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, IconButton, Button } from "@mui/material";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 
 function BookList() {
   const [books, setBooks] = useState([]);
@@ -16,40 +19,91 @@ function BookList() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 2,
-        gap: 2,
+        height: "100vh"
       }}
     >
-      <Typography
-        variant="h2"
-        component="h2"
-        sx={{ color: "#d32f2f", margin: 2 }}
+      <Box sx={{
+        display: "flex",
+        justifyContent: "flex-end",
+        marginTop: 2
+      }}>
+        <IconButton color="error">
+          <AddCircleIcon sx={{fontSize: 40}} />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          padding: 2,
+          gap: 2,
+          overflowX: 'auto',
+          marginBottom: "100px"
+        }}
       >
-        BOOK LIBRARY
-      </Typography>
-      {books.map((book) => (
         <Box
-          key={book.id}
           sx={{
-            border: "1px solid grey",
-            borderRadius: "10px",
-            padding: 2,
-            width: "75%",
-            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            color: "#d32f2f"
           }}
         >
-          <Typography variant="h5" component="h2">
-            {book.title}
+          <Typography
+            variant="h2"
+            component="h2"
+            sx={{ color: "#d32f2f", margin: 2, fontSize: 30 }}
+          >
+            BOOK LIBRARY
           </Typography>
-          <Typography variant="subtitle1">Author: {book.author}</Typography>
-          <Typography variant="subtitle1">
-            Year Published: {book.yearPublished}
-          </Typography>
-          <Typography variant="subtitle1">Genre: {book.genre}</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: 10
+            }}
+          >
+            <Typography
+              variant="h2"
+              component="h2"
+              sx={{ color: "#d32f2f", margin: 2, fontSize: 30 }}
+            >
+              Scroll Here
+            </Typography>
+            <ArrowCircleRightIcon sx={{fontSize: 50}} />
+          </Box>
         </Box>
-      ))}
+        {books.map((book) => (
+          <Box
+            key={book.id}
+            sx={{
+              position: "relative",
+              border: "1px solid grey",
+              borderRadius: "10px",
+              padding: 2,
+              width: "50vh",
+              height: "80%",
+              backgroundColor: "white",
+              flexShrink: 0
+            }}
+          >
+            <IconButton color="error" sx={{position: "absolute", right: 5, top: 5}}>
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h5" component="h2">
+              {book.title}
+            </Typography>
+            <Typography variant="subtitle1">Author: {book.author}</Typography>
+            <Typography variant="subtitle1">
+              Year Published: {book.yearPublished}
+            </Typography>
+            <Typography variant="subtitle1">Genre: {book.genre}</Typography>
+            <Button color="error" variant="contained">Edit</Button>
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 }
